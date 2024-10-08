4049d9497973b06c3e4bbac1da956cbad1c77679
Vrush2004 <vrushalichaube@gmail.com>
2024-10-08 20:24:07 +0530

    fibonacci series

--- Simple Programs/FibonacciSeries.py
+++ Simple Programs/FibonacciSeries.py
@@ -27,3 +27,15 @@ else:
 
 for i in range(num):
     print(fibonacciSeries(i), end=" ")
+
+# Method 3: Using direct formulae
+# Fn = {[(√5 + 1)/2] ^ n} / √5
+import math
+def fibonacciSeries(phi, n):
+    for i in range(0, n+1):
+        result = round(pow(phi, i) / math.sqrt(5))
+        print(result, end=" ")
+
+num = 10
+phi= (1 + math.sqrt(5)) / 2
+fibonacciSeries(phi, num)
